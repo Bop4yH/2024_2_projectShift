8e0a23a4d1638d7922fc4ad76a936756a8ca6d51
Кузьмин Александр Владимирович <aleksandr.kuzmin2000@gmail.com>
2024-10-03 20:36:59 +0700

    fix

--- src/main/java/ru/shift/Main.java
+++ src/main/java/ru/shift/Main.java
@@ -3,6 +3,6 @@ package ru.shift;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println('w');
+        System.out.println('q');
     }
 }
