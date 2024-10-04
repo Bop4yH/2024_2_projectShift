a4f45a74075157457c1cb9b02ac6f730770844ab
Кузьмин Александр Владимирович <aleksandr.kuzmin2000@gmail.com>
2024-10-04 14:02:20 +0700

    Поправил используя форматированный вывод

--- task1/src/main/java/ru/shift/Main.java
+++ task1/src/main/java/ru/shift/Main.java
@@ -15,20 +15,14 @@ public class Main {
     public static void printTable(int x) {
         final int squareLength = String.valueOf(x * x).length();
         final int length = String.valueOf(x).length();
-
-        StringBuilder line;
-        final StringBuilder sep = new StringBuilder("-".repeat(length))
-                .append(("+" + "-".repeat(squareLength)).repeat(x));
+        final String sep = "-".repeat(length) + ("+" + "-".repeat(squareLength)).repeat(x);
 
         for (int i = 0; i <= x; i++) {
-            line = new StringBuilder(" ".repeat(length - String.valueOf(i).length()) + (i == 0 ? " " : i));
-
+            System.out.printf("%" + length + "s", i == 0 ? "" : i);
             for (int j = 1; j <= x; j++) {
-                line.append("|")
-                        .append(" ".repeat(squareLength - String.valueOf((i == 0 ? 1 : i) * j).length()))
-                        .append((i == 0 ? 1 : i) * j);
+                System.out.printf("|" + "%" + squareLength + "d", (i == 0 ? 1 : i) * j);
             }
-            System.out.println(line);
+            System.out.println();
             System.out.println(sep);
         }
     }
